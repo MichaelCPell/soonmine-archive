331282b6e0568b40a64a5f75a92416d6fb16b7ab
Michael Pell <MichaelCPell@gmail.com>
2013-07-08 14:45:51 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soonmine::Application.config.secret_key_base = 'd5176ae033d2e95ec9fa3aa99fd3c71ec8199d20c60f1abcff9a39aa6f21376c78b930c8e98e442eed9d25d9272bbbcd36a47328a8721f1bb670b75eb85f6c8c'
