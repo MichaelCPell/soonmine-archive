da424e2e7afb625725c835f28f4a886ad5ed30da
Michael Pell <MichaelCPell@gmail.com>
2013-07-09 12:16:05 -0600

    Gave up Testing RegistrationsController, Added Devise Views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:firstname, :lastname, :email, :password, :password_confirmation) 
+    end
   end
 end
--- spec/controllers/devise/sessions_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require "spec_helper"
-
-describe SessionsController do
-  describe "POST create" do 
-    
-  end
-  
-end
