dac87e14b3f29c18b3bc1829a57421e38f57635c
Michael Pell <MichaelCPell@gmail.com>
2013-07-10 16:33:29 -0600

    Adds nav functionality and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Soonmine::Application.routes.draw do
   resources :users
 
 
+  get "/about", to: "static_pages#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
