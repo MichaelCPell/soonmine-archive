b6d652213b710de25812ff52940153d8ae2203bb
Michael Pell <MichaelCPell@gmail.com>
2013-07-17 13:19:22 -0600

    Updates External for Color

--- public/external.js
+++ public/external.js
@@ -1,1 +1,4 @@
-alert(1)
+var newDivContent = "<div style='width: 100px; height: 100px; background: #298DFF;'>Hello World</div>"
+var newDiv = document.createElement("div")
+newDiv.innerHTML = newDivContent;
+document.getElementsByTagName("body")[0].appendChild(newDiv);
