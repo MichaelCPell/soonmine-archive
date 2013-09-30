d18d28acdd8d5db17ba1fbe09d36adc12df56096
Michael Pell <MichaelCPell@gmail.com>
2013-09-30 12:39:46 -0600

    Permits Users to See User#Show if not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :check_for_email, except: :index
+  before_filter :check_for_email, except: [:index, :show]
   
   def show
     @user = User.find(params[:id])
