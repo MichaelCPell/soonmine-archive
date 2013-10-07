fbbe142003bde6a9b2a0cf70debda8cad0760d61
Michael Pell <MichaelCPell@gmail.com>
2013-10-07 13:44:36 -0600

    Users Ignore Friendly_id in Active Admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,12 @@
 ActiveAdmin.register User do
+
+  before_filter do 
+    User.class_eval do
+      def to_param
+        id.to_s
+      end
+    end
+  end
   index do
     column :email
     column :current_sign_in_at
